7a04727f1884019f05bf19a781628f9d0e8452dd
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-20 21:41:16 +0100

    basic build in types

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -9,7 +9,30 @@ namespace FirstProject // Note: actual namespace depends on the project name.
             Console.Write("Please enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine("Hello " + name);
+
+            string someText = "Some text";
+
+            char jChar = 'j';
+            char jCharUnicode = '\u006A';
+
+            bool isUserReady = true;
+
+            DateTime now = DateTime.Now;
+            DateTime dateOfBirt = new DateTime(1982, 8, 25);
+            Console.Write("Date of birth: " + dateOfBirt);
+
+            byte byteNumber = 200;
+
+            float floatNumber = 1.5F;
+            decimal decimalNumber = 1.5M;
+            double doubleNumber = 1.5;
+
+
+
+
             Console.ReadLine();
+
+
         }
     }
 }
