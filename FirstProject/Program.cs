2870e4b65bdee165c03a9ccfe1f527c436b41d57
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-20 21:18:54 +0100

    interact with console

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -6,7 +6,10 @@ namespace FirstProject // Note: actual namespace depends on the project name.
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.ReadLine();
         }
     }
 }
