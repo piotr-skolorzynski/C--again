b76e5cb4bef95129e04effad6c21ab51e51ed3c0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-20 22:12:24 +0100

    example how to use strings and string builder

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace FirstProject // Note: actual namespace depends on the project name.
 {
@@ -6,33 +7,49 @@ namespace FirstProject // Note: actual namespace depends on the project name.
     {
         private static void Main(string[] args)
         {
-            Console.Write("Please enter your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name);
+            string message1;
+            message1 = "some value";
 
-            string someText = "Some text";
+            string message2 = null;
 
-            char jChar = 'j';
-            char jCharUnicode = '\u006A';
+            string message3 = string.Empty; //reprezentuje wartość brak znaków
+            // string.Empty != null
 
-            bool isUserReady = true;
+            string text = "He sadi \"Hi\"";
+            //znak \ przed cudzysłowiem oznacza że chcemy żeby wyświetlono cudzysłów a nie żeby
+            // kompilator traktował go jako znak specjlany
 
-            DateTime now = DateTime.Now;
-            DateTime dateOfBirt = new DateTime(1982, 8, 25);
-            Console.Write("Date of birth: " + dateOfBirt);
+            string widowLocation = "c:\\windows";
+            //powyżej tak samo jesli chcemy żeby był wyświetlony znak \ to przed nim umieszczono
+            // \ jako znak specjalny
 
-            byte byteNumber = 200;
+            //powyższy zapid jest upierdliwy więc stworzono stringi dosowe tzw verbatim string
+            string fontsFolder = @"c:\windows\fonts";
+            // jedynym wyjątkiem jest cudzysłów, dla niego nawet verbatim nie pomoże
 
-            float floatNumber = 1.5F;
-            decimal decimalNumber = 1.5M;
-            double doubleNumber = 1.5;
+            //konkatenacja
+            //wykorzystanie metody concat, można dodać więcej argumentów, należy oddzielać je przecinkami
+            string concatenated = string.Concat(text, " to me");
 
+            // można to też robić poprzez używanie operatora +
+            string concatenated2 = text + " to " + "me";
 
+            //interpolacja stringów (coś podobnego do template string)
+            string interpolated  = $"{text} to me";
+            //należy jednak pamiętać że interpolowany string zajmuje nowe miejsce w pamięci więc wielokrotne interpolowanie 
+            // tworzy nowe zmienne i pożera pamięć.
 
+            //dlatego wymyślono string buildera żeby dynamicznie powiększał wartość dla zmiennej a nie tworzył nowej
+            StringBuilder sb = new StringBuilder("This"); // teraz można powiększać stringa
+            sb.Append(" is");
+            sb.Append(" a");
+            sb.Append(" long");
+            sb.Append(" text");
+
+            string result = sb.ToString();
+            Console.WriteLine("String builder result: " + result);
 
             Console.ReadLine();
-
-
         }
     }
 }
