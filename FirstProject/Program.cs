0c895168f822ecf6ead37db0d58a68afa8f83565
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-21 14:20:33 +0100

    if statements

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace FirstProject // Note: actual namespace depends on the project name.
 {
@@ -7,85 +6,24 @@ namespace FirstProject // Note: actual namespace depends on the project name.
     {
         private static void Main(string[] args)
         {
-            int result;
-            int x = 10;
-            int y = 5;
+            Console.Write("Year of birth? ");
+            string userInput = Console.ReadLine();
 
-            //Kolejność działań matematycznych jest zachowana 
-            int value = 2 + 2 * 5; //12
-            Console.WriteLine("kolejność działań zachowana dla przykładu 2 + 2 * 5 = " + value);
+            bool isOver18 = DateTime.Now.Date.Year - int.Parse(userInput) > 18; 
 
-            result = (x + y);
-            Console.WriteLine("Additional Operator: x + y = " + result);
+            if (isOver18)
+            {
+                Console.WriteLine("hello");
+            }
+            else if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Oh well its Sunday");
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+            }
 
-            result = (x - y);
-            Console.WriteLine("Substraction Operator: x - y = " + result);
-
-            result = (x * y);
-            Console.WriteLine("Multiplication Operator: x * y = " + result);
-
-            result = (x / y);
-            Console.WriteLine("Division Operator: x / y = " + result);
-
-            result = (x % y);
-            Console.WriteLine("Modulo Operator: x % y = " + result);
-
-            int value1 = 10;
-            int value2 = 10;
-
-            Console.WriteLine("Pre-increment result:");
-            Console.WriteLine(++value1);
-
-            Console.WriteLine("Post-increment result:");
-            Console.WriteLine(value2++);
-            Console.WriteLine(value2);
-
-            bool boolResult;
-            x = 2;
-            y = 10;
-
-            Console.WriteLine("x: " + x);
-            Console.WriteLine("y: " + y);
-
-            boolResult = (x == y);
-            Console.WriteLine("Equal to Operator: (x == y) = " + boolResult);
-
-            boolResult = (x > y);
-            Console.WriteLine("Greater than Operator: (x > y) = " + boolResult);
-
-            boolResult = (x < y);
-            Console.WriteLine("Less than Operator: (x == y) = " + boolResult);
-
-            boolResult = (x >= y);
-            Console.WriteLine("Greater than or Equal to Operator: (x >= y) = " + boolResult);
-
-            boolResult = (x <= y);
-            Console.WriteLine("Less than or Equal to Operator: (x <= y) = " + boolResult);
-
-            boolResult = (x != y);
-            Console.WriteLine("Not Equal to Operator: (x == y) = " + boolResult);
-
-            bool a = true;
-            bool b = false;
-
-            Console.WriteLine("a: " + a);
-            Console.WriteLine("b: " + b);
-            
-            boolResult = a && b;
-            Console.WriteLine("AND Operator: (a && b) = " + boolResult);
-
-            boolResult = (a || b);
-            Console.WriteLine("OR Operator: (a || b) = " + boolResult);
-
-            boolResult = (!a);
-            Console.WriteLine("NOT Operator: !a = " + boolResult);
-            
-            
-            int greaterNumber = x > y ? x : y;
-            Console.WriteLine("Ternary conditional operator: x > y ? x : y = " + greaterNumber);
-            
-            
-            Console.ReadLine();
         }
     }
 }
