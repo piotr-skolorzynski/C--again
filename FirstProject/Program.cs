fdebf7a266f7a8ec4973a9b68964341cbeaad6e4
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-22 21:01:18 +0100

    try - catch - finally

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -6,23 +6,24 @@ namespace FirstProject
     {
         private static void Main(string[] args)
         {
-            /*
-                Jeżeli mamy sytuację że wyświetlamy jakąś wartość a może być że jeszcze ona nie została 
-                zadeklarowana to otrzymamy błąd. więc można zastosować np. flagę i ternary operator
+            string[] cars = { "Volvo", "BMW", "Mazda" };
 
-                int favoriteNumber = 23;
-                bool favoriteNumberHasValue = false;
-
-                Console.WriteLine("Favorite number: " + (favoriteNumberHasValue ? favoriteNumber.ToString() : ""));
-
-                Żeby uniknąć takiego rozwlekłego kodu wymyślono nullable
-                można taką zmienną deklarować jako Nullable<int> lub krócej int?
-            */
-
-            int? favoriteNumber = 23;
-            // Console.WriteLine("Favorite number: " + (favoriteNumber != null ? favoriteNumber.ToString() : ""));
-            // lub
-            Console.WriteLine("Favorite number: " + (favoriteNumber.HasValue ? favoriteNumber.Value.ToString() : ""));
+            try
+            {
+                cars[4] = "Tesla"; //throws System.IndexOutOfRangeException
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Handling IndexOutOfRangeException");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Handling any exception");
+            }
+            finally
+            {
+                Console.WriteLine("Cleanup");
+            }
         }
     }
 }
