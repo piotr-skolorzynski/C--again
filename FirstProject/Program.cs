f057d602fff29839ea98055408c562a7c6594669
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-28 22:11:51 +0100

    how to use var

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -18,17 +18,12 @@ namespace FirstProject
 
         };
       }
-
       private static void Main(string[] args)
       {
-        Dictionary<string, Currency> currencies = GetCurrencies();
+        var currencies = GetCurrencies();
         Console.WriteLine("Check the rate for: ");
-        string userInput = Console.ReadLine();
+        var userInput = Console.ReadLine();
 
-        // //pobieranie wartości ze słownika
-        // Currency selectedCurrency = currencies[userInput];
-        //żeby się zabezpieczyć przed sytuacją że w słowniku nie ma wpisanej przez użytkownika wartości
-        //można skorzystać z metody TryGetValue dla słownika
         Currency selectedCurrency = null;
         if (currencies.TryGetValue(userInput, out selectedCurrency)) 
         {
@@ -38,17 +33,8 @@ namespace FirstProject
         {
           Console.WriteLine("Currency not found");
         }
-
-        //usuwanie elementu ze słownika
         currencies.Remove("usd");
-
-        //dodawanie elementów do słownika (klucz, wartość)
         currencies.Add("pln", new Currency("pln", "Polski Nowy", 4.5));
-
-        //Jeżeli będę chciał dodać element do słownika i klucz będzie się powtarzał to 
-        // otrzymamy błąd Dlatego bezpieczniejszą metodą dodania klucza jest użycie metody TryAdd. Przesyła się tak samo 
-        // oba argumenty klucz - wartość ale ponadto sama metoda zwraca wartość logiczną czy udało się dodać element 
-        // do słownika
         currencies.TryAdd("usd", new Currency("usd", "Dollar", 1));
       }
     }
