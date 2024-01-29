396f52ece49d99b9307367b57498fafe04f2e525
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-29 22:25:03 +0100

    example of iterator

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -5,37 +5,28 @@ namespace FirstProject
 {
     internal class Program
     {
-      static Dictionary<string, Currency> GetCurrencies()
+      public static IEnumerable<int> GetYieldedData()
       {
-        return new Dictionary<string, Currency>
+        //przykład iteratora zwracającego liczbę całkowitą
+        for (int i = 0; i <= 9; i++)
         {
-          { "usd", new Currency("usd", "United States Dollar", 1)},
-          { "eur", new Currency("eur", "Euro", 0.83975)},
-          { "gbp", new Currency("gbp", "British Pound", 0.74771)},
-          { "cad", new Currency("cad", "Canadian Dollar", 1.30724)},
-          { "inr", new Currency("inr", "Indian Rupee", 73.04)},
-          { "mxn", new Currency("mxn", "Mexican Peso", 21.7571)},
-
-        };
+          yield return i;
+          if (i % 3 == 0) 
+          {
+            //możliwe jest zakończenie działania iteratora 
+            //w przypadku spełnienia konkretnego warunku
+            yield break;
+          }
+        }
       }
       private static void Main(string[] args)
       {
-        var currencies = GetCurrencies();
-        Console.WriteLine("Check the rate for: ");
-        var userInput = Console.ReadLine();
-
-        Currency selectedCurrency = null;
-        if (currencies.TryGetValue(userInput, out selectedCurrency)) 
+        //przykład wykorzystania iteratora
+        var yieldedData = GetYieldedData();
+        foreach (int element in yieldedData)
         {
-          Console.WriteLine($"Rate for USD to {selectedCurrency.FullName} is {selectedCurrency.Rate}");
+          Console.WriteLine(element); //0 1 2 3 4 5 6 7 8 9
         }
-        else 
-        {
-          Console.WriteLine("Currency not found");
-        }
-        currencies.Remove("usd");
-        currencies.Add("pln", new Currency("pln", "Polski Nowy", 4.5));
-        currencies.TryAdd("usd", new Currency("usd", "Dollar", 1));
       }
     }
 }
