fd956c53a4fddc17a5ce2b1e660e67e9d4ffc7df
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-03-10 14:49:13 +0100

    filter data when fetching collection

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -14,10 +14,43 @@ namespace FirstProject
   {
       static void Main(string[] args)
       {
-          string csvPath = @"CSV_PATH";
+          string csvPath = @"D:\Repos\26. C# basics\FirstProject\googleplaystore.csv";
           var googleApps = LoadGoogleAps(csvPath);
 
-          Display(googleApps);
+        //   Display(googleApps);
+        GetData(googleApps);
+      }
+
+      static void GetData(IEnumerable<GoogleApp> googleApps)
+      {
+        var highRatedBeautyApps = googleApps.Where(app => app.Rating > 4.6 && app.Category == Category.BEAUTY);
+        Display(highRatedBeautyApps);
+
+        //first element from collection
+        var firtsHighRatedBeautyApp = highRatedBeautyApps.First();
+        Console.WriteLine("First High Rated App");
+        Display(firtsHighRatedBeautyApp);
+
+        //first element from collection filtered by predicate sent to First operator
+        // var firstHighRatedBeautyWithLowReviews = highRatedBeautyApps.First(app => app.Reviews < 300);
+        // Console.WriteLine("First High Rated App with low reviews");
+        // Display(firstHighRatedBeautyWithLowReviews);
+
+        //if we do not want to have axception it is possible to replace First with FirstOrDefault operator and 
+        // in case of no match default value will be null
+        // var firstHighRatedBeautyWithLowReviews = highRatedBeautyApps.FirstOrDefault(app => app.Reviews < 50);
+        // Console.WriteLine("First High Rated App with low reviews");
+        // Display(firstHighRatedBeautyWithLowReviews);
+
+        //Operator single
+        // var firstHighRatedBeautyWithLowReviews = highRatedBeautyApps.Single(app => app.Reviews < 50);
+        var firstHighRatedBeautyWithLowReviews = highRatedBeautyApps.SingleOrDefault(app => app.Reviews < 50);
+        Console.WriteLine("First High Rated App with low reviews");
+        Display(firstHighRatedBeautyWithLowReviews);
+
+        // If we do not want to have no matching exception you can use SingleOrDefault operator
+
+        //Operator Last - similiar to First, returns last element from collection and also has it version LastOrDefault
       }
 
       static void Display(IEnumerable<GoogleApp> googleApps)
